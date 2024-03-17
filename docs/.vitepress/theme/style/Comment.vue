54f3690caa1be80ad050cd5c8924496a0547891b
markbang <3137972908@qq.com>
2024-03-17 17:43:21 +0800

    update

--- docs/.vitepress/theme/style/Comment.vue
+++ docs/.vitepress/theme/style/Comment.vue
@@ -20,7 +20,7 @@ const init = () => {
       admin: ['markbang'], // GitHub repo 所有者
       labels: ['GitTalk'], // GitHub issue 标签
       proxy:
-        'https://mellifluous-bombolone-049a57.netlify.app/github_access_token',
+        'https://cors-server-ecru.vercel.app/github_access_token',
       createIssueManually: true //如果当前页面没有相应的 issue 且登录的用户属于 admin，则会自动创建 issue。如果设置为 true，则显示一个初始化页面，创建 issue 需要点击 init 按钮。
     })
     gitTalk.render('gitalk-page-container')
